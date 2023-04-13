35ef8ee2dd2b675cee7a4423cfa788323a8572fe
Aryan Sharma <aryan.sharma21@st.niituniversity.in>
2023-04-13 15:08:34 +0530

    Add files via upload

--- priority.py
+++ priority.py
@@ -1,81 +1,71 @@
-from tabulate import tabulate
+processes = [("p1", 0, 4, 2),
+             ("p2", 1, 3, 3),
+             ("p3", 2, 1, 4),
+             ("p4", 3, 5, 5),
+             ("p5", 4, 2, 5)]
 
-# Define a process class to hold information about each process
-class Process:
-    def __init__(self, name, priority, burst_time):
-        self.name = name
-        self.priority = priority
-        self.burst_time = burst_time
-        self.waiting_time = 0
-        self.turnaround_time = 0
-        self.response_time = -1
+# sort the processes based on their arrival time
+processes.sort(key=lambda x: x[1])
 
-# Define a function to simulate the CPU scheduler
-def priority_scheduling(process_list):
-    # Sort the process list by priority
-    process_list.sort(key=lambda x: x.priority)
-    
-    # Initialize the current time, total waiting time, total turnaround time, and total response time
-    current_time = 0
-    total_waiting_time = 0
-    total_turnaround_time = 0
-    total_response_time = 0
-    
-    # Create the Gantt chart
-    gantt_chart = []
-    
-    # Loop through the process list and simulate the CPU scheduler
-    for process in process_list:
-        # Add the process name to the Gantt chart
-        gantt_chart.append([process.name, current_time])
-        
-        # Calculate the response time
-        if process.response_time == -1:
-            process.response_time = current_time
-        
-        # Run the process
-        current_time += process.burst_time
-        waiting_time = current_time - process.burst_time
-        process.waiting_time = waiting_time
-        total_waiting_time += waiting_time
-        
-        # Add the process duration to the Gantt chart
-        gantt_chart[-1].append(current_time)
-        
-        # Calculate the turnaround time
-        turnaround_time = current_time
-        process.turnaround_time = turnaround_time
-        total_turnaround_time += turnaround_time
-        
-        # Calculate the response time
-        total_response_time += process.response_time
-    
-    # Calculate the average waiting time, turnaround time, and response time
-    avg_waiting_time = total_waiting_time / len(process_list)
-    avg_turnaround_time = total_turnaround_time / len(process_list)
-    avg_response_time = total_response_time / len(process_list)
-    
-    # Create a list of lists containing the process metrics
-    process_metrics = []
-    for process in process_list:
-        process_metrics.append([process.name, process.priority, process.burst_time, process.waiting_time, process.turnaround_time - process.response_time, process.response_time])
-    
-    # Print the table and Gantt chart
-    print("Process metrics:")
-    print(tabulate(process_metrics, headers=["Process name", "Priority", "Burst time", "Waiting time", "Response time", "Turnaround time"]))
-    print("Average waiting time:", avg_waiting_time)
-    print("Average turnaround time:", avg_turnaround_time)
-    print("Average response time:", avg_response_time)
-    print("Gantt chart:")
-    print(tabulate(gantt_chart, headers=["Process name", "Start time", "End time"]))
+n = len(processes)
+# initialize the completion times, turnaround times, and waiting times
+ct = [0] * n
+tat = [0] * n
+wt = [0] * n
+rt = [0] * n
 
-# Example usage:
-process_list = [
-    Process("P1", 2, 5),
-    Process("P2", 1, 3),
-    Process("P3", 4, 8),
-    Process("P4", 3, 4),
-]
+# initialize the remaining burst times and the current time
+remaining_bt = [processes[i][2] for i in range(n)]
+current_time = 0
 
-priority_scheduling(process_list)
+# initialize a list to keep track of the processes that have been completed
+completed = []
 
+# loop until all processes have been completed
+while len(completed) < n:
+    # find the process with the highest priority and the lowest remaining burst time
+    highest_priority = -1
+    lowest_bt = float('inf')
+    selected_process = -1
+    for i in range(n):
+        if processes[i] not in completed and processes[i][1] <= current_time:
+            if processes[i][3] > highest_priority or (processes[i][3] == highest_priority and remaining_bt[i] < lowest_bt):
+                highest_priority = processes[i][3]
+                lowest_bt = remaining_bt[i]
+                selected_process = i
+                
+    if selected_process == -1:
+        # if no process can be selected, increment the current time
+        current_time += 1
+    else:
+        # execute the selected process for one time slice
+        remaining_bt[selected_process] -= 1
+        current_time += 1
+        # if the process has completed execution, update the completion time and add it to the completed list
+        if remaining_bt[selected_process] == 0:
+            ct[selected_process] = current_time
+            completed.append(processes[selected_process])
+            
+# calculate the turnaround times and waiting times
+for i in range(n):
+    tat[i] = ct[i] - processes[i][1]
+    wt[i] = tat[i] - processes[i][2]
+    rt[i] = ct[i] - processes[i][1] - processes[i][2]
+
+# print the results
+print("Process\tAT\tBT\tPriority\tCT\tTAT\tWT\tRT")
+total_tat = 0
+total_wt = 0
+total_rt = 0
+for i in range(n):
+    print(f"{processes[i][0]}\t{processes[i][1]}\t{processes[i][2]}\t{processes[i][3]}\t\t{ct[i]}\t{tat[i]}\t{wt[i]}\t{rt[i]}")
+    total_tat += tat[i]
+    total_wt += wt[i]
+    total_rt += rt[i]
+
+avg_tat = total_tat / n
+avg_wt = total_wt / n
+avg_rt = total_rt / n
+print(f"\nAverage turnaround time: {avg_tat:.2f}")
+print(f"Average waiting time: {avg_wt:.2f}")
+print(f"Average response time: {avg_rt:.2f}")
